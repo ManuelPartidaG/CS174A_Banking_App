f9f5f91ba4bca33e17e879296356dbb63adc0432
manuelpartidag <manuelpartidagomez@umail.ucsb.edu>
2019-11-18 19:20:27 -0800

    edited User.java to add customers to the Customer table

--- User.java
+++ User.java
@@ -1,36 +1,34 @@
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.*;
 import java.util.Properties;
 
 import oracle.jdbc.pool.OracleDataSource;
 import oracle.jdbc.OracleConnection;
 import java.sql.DatabaseMetaData;
 
+public class User {
+  // The recommended format of a connection URL is the long format with the
+  // connection descriptor.
+  final static String DB_URL= "jdbc:oracle:thin:@cs174a.cs.ucsb.edu:1521/orcl";
 
-public class User{
-//private String taxid;
-//private byte pinHash[];
-//private Arraylist<Account> accounts;
+  final static String DB_USER = "c##manuelpartidagomez";
+  final static String DB_PASSWORD = "9599663";
 
-final static String DB_URL= "jdbc:oracle:thin:@cs174a.cs.ucsb.edu:1521/orcl";
-
-final static String DB_USER = "c##manuelpartidagomez";
-final static String DB_PASSWORD = "9599663";
-
-
-
-
-  public static void main(String args[]) throws SQLException, ClassNotFoundException {
-
-    try {Class.forName("oracle.jdbc.driver.OracleDriver");}
-    catch (ClassNotFoundException e){
-      System.out.println("Something went wrong1.");
-    }
+ /*
+  * The method gets a database connection using
+  * oracle.jdbc.pool.OracleDataSource. It also sets some connection
+  * level properties, such as,
+  * OracleConnection.CONNECTION_PROPERTY_DEFAULT_ROW_PREFETCH,
+  * OracleConnection.CONNECTION_PROPERTY_THIN_NET_CHECKSUM_TYPES, etc.,
+  * There are many other connection related properties. Refer to
+  * the OracleConnection interface to find more.
+  */
+  public static void main(String args[]) throws SQLException {
     Properties info = new Properties();
     info.put(OracleConnection.CONNECTION_PROPERTY_USER_NAME, DB_USER);
     info.put(OracleConnection.CONNECTION_PROPERTY_PASSWORD, DB_PASSWORD);
@@ -41,45 +39,26 @@ final static String DB_PASSWORD = "9599663";
     ods.setURL(DB_URL);
     ods.setConnectionProperties(info);
 
+    // With AutoCloseable, the connection is closed automatically.
+    try (OracleConnection connection = (OracleConnection) ods.getConnection()) {
+      // Get the JDBC driver name and version
+      DatabaseMetaData dbmd = connection.getMetaData();
 
-    try (OracleConnection connection = (OracleConnection) ods.getConnection()){
-      printCustomers(connection);
+      AddEmployees(connection);
+    }
+  }
+ /*
+  * Adds employee to the employees table.
+  */
+  public static void AddEmployees(Connection connection) throws SQLException {
+    // Statement and ResultSet are AutoCloseable and closed automatically.
+    String sql = "INSERT INTO Customer " +
+                   "VALUES (42000,'Yovany Mejia',2020,'6749 Trigo Rd Unit B')";
+    try (Statement statement = connection.createStatement()) {
+    //ResultSet resultSet = statement;
+    statement.executeUpdate(sql);
+    System.out.println("successful insert value");
 
     }
-
-    catch (SQLException e) {
-      System.out.println("Something went wrong2.");
-    }
-}
-
-
-public static void printCustomers(Connection connection) throws SQLException {
-  // Statement and ResultSet are AutoCloseable and closed automatically.
-
-  try {
-  PreparedStatement statement = connection.prepareStatement(" instert into customer (taxid,name,pin,address) values (?,'?',?,'?')");
-    try {
-      //ResultSet resultSet = statement
-      //  .executeQuery("INSERT INTO Customer (TAXID,NAME,PIN,ADDRESS) VALUES (9599663,'Manuel Partida ',4546,'6749 Trigo Unit B');");
-
-
-
-      statement.setInt (1, 9599663);
-      statement.setString (2, "Manuel Partida Gomez");
-      statement.setInt   (3, 1010);
-      statement.setString(4, "6749 Trigo Rd Unit B");
-
-      ResultSet answerSet = statement.executeQuery();
-      connection.close();
-    }
-
-catch (SQLException e) {
-  System.out.println("Something went wrong3.");
-
-}
-
-} catch (SQLException e) {
-  System.out.println("Something went wrong4.");
-}
-}
+  }
 }
